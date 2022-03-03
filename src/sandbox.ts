dfc4cdae0e253d6c61b39660c157e55167ce7079
Ajuka David <ajuka.zephiniah@gmail.com>
2022-03-03 13:54:56 +0100

    did more type aliases

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -4,8 +4,35 @@
 
 //
 
-let greet: Function;
+// let greet: Function;
 
-greet = () => {
-    console.log('hello again')
-}
+// greet = () => {
+//     console.log('hello again')
+// }
+
+// const add = (a: number, b: number, c: number | string = 10) => {
+//     console.log(a + b)
+//     console.log(c)
+// }
+// add(5, 10, '20')
+
+// const minus = (a: number, b: number): number => {
+//     return a + b
+// }
+
+// let result = minus(10, 7)
+
+
+/*TYPE ALIAASES */
+
+// type StringOrNum = string | number
+// type objWithName = {name: string, uid: StringOrNum}
+
+
+// const logDetails = (uid: StringOrNum, item: string) => {
+//     console.log(`${item} has a uid of ${uid}`)
+// }
+
+// const greet = (user: {name: objWithName}) => {
+//     console.log(`${user.name} says hello`)
+// }
